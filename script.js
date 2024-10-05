6e954cfa345b990f85d7cc41c8cf39b0bd4fa1ca
Federico Angarella <164171052+fortigate3600@users.noreply.github.com>
2024-10-05 11:49:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ const start = 500;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+for (let i = 0; i < 100; i++) {
+  ctx.beginPath();
+  ctx.moveTo(1000, 0);
+  ctx.lineTo(1000, 800);
+  ctx.stroke();
+}
+
 // Funzione per generare un numero casuale tra min e max
 function getRandomInt() {
   return Math.random();
 }
+
+let diz = {};
 
 // Disegnare un numero definito di linee casuali
 function drawRandomLines() {
@@ -23,8 +32,8 @@ function drawRandomLines() {
   ctx.moveTo(0, start - h);
   ctx.lineTo(path, start - h);
   ctx.stroke();
-
-  for (let i = 1; i <= m; i++) {
+  let i = 1;
+  for (; i <= m; i++) {
     const prob = getRandomInt();
     if (prob < p) {
       h += heightpath;
@@ -40,6 +49,13 @@ function drawRandomLines() {
     ctx.lineTo((i + 1) * path, start - h);
     ctx.stroke();
   }
+
+  if (!(h in diz)) {
+    diz[h] = 1;
+  } else {
+    diz[h]++;
+  }
+
   return h / heightpath;
 }
 
@@ -50,13 +66,26 @@ GoButton.addEventListener("click", function () {
   p = parseFloat(document.getElementById("prob").value);
   m = parseInt(document.getElementById("systmes").value);
   n = parseInt(document.getElementById("atks").value);
-  path = canvas.width / (m + 1);
+  path = (canvas.width - 200) / (m + 1);
   heightpath = canvas.height / m;
+  diz = {};
   numcampioni += 1;
   for (let i = 0; i < n; i++) {
     let nbreach = drawRandomLines();
     sumbreaches += nbreach;
   }
+
+  const maxValue = Math.max(...Object.values(diz));
+  console.log(maxValue);
+  z = 200 / maxValue;
+
+  for (h in diz) {
+    ctx.beginPath();
+    ctx.moveTo(m * path, start - h);
+    ctx.lineTo(m * path + z * diz[h], start - h);
+    ctx.stroke();
+  }
+
   var mediacolpiti = sumbreaches / (n * numcampioni);
   textmediacolpiti.innerText =
     "Media sistemi compromessi: " + mediacolpiti.toFixed(2);
@@ -68,4 +97,10 @@ clearButton.addEventListener("click", function () {
   sumbreaches = 0;
   numcampioni = 0;
   textmediacolpiti.innerText = "";
+  for (let i = 0; i < 100; i++) {
+    ctx.beginPath();
+    ctx.moveTo(1000, 0);
+    ctx.lineTo(1000, 800);
+    ctx.stroke();
+  }
 });
